105cbb83e43c96cc156bcc777400ac9f37a8e670
iamagni <46749961+iamagni@users.noreply.github.com>
2019-02-24 17:12:06 +0530

    Update testng.bat

--- testng.bat
+++ testng.bat
@@ -1,4 +1,4 @@
 set projectLocation=E:\driver\JavaWorkspace\AutomationTest1
 cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+set classpath=%projectLocation%\bin;
 java org.testng.TestNG %projectLocation%\testng.xml
